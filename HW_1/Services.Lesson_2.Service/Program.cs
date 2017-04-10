2b995159f2306947753e662b107191d74b91c2da
VV <VV@vv>
2017-04-10 22:29:19 +0500

    add project

---
+++ HW_1/Services.Lesson_2.Service/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace Services.Lesson_2.Service
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var host = new ServiceHost(typeof(Library), new Uri("http://localhost:8080/library"));
+            //передаем реализацию сервиса и базовый адрес
+
+            host.Open();
+
+            Console.WriteLine("Press enter...");
+            Console.ReadLine();
+
+            host.Close();
+        }
+    }
+}
+
+
